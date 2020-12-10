6b0e7ca54b9729fec468f1f63749d79f65f558db
Linggar Primahastoko <linxboyz@gmail.com>
2020-12-10 16:34:16 +0700

    Add config sample and dataset folder

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from app.dashboard import dashboard
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
+
+app.config.from_object('config')
 
 # register app
 app.register_blueprint(dashboard)
