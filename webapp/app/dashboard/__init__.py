084fcae508aebe46b0dfeefeee16234c207b09e2
Linggar Primahastoko <linxboyz@gmail.com>
2020-12-10 17:37:59 +0700

    Add adminlte for dashboard templates

--- webapp/app/dashboard/__init__.py
+++ webapp/app/dashboard/__init__.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 
 @dashboard.route('/')
 def index():
-    return 'Hai'
+    return render_template('dashboard/index.html')
