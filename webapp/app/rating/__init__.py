e22ba91d4bf6639384f90aea774af1cad600b77c
Linggar Primahastoko <linxboyz@gmail.com>
2020-12-21 19:05:22 +0700

    Complete rating page

--- webapp/app/rating/__init__.py
+++ webapp/app/rating/__init__.py
@@ -9,4 +9,40 @@ rating = Blueprint('rating', __name__, url_prefix='/rating')
 @rating.route('/')
 @login_required
 def index():
-    return render_template('rating/index.html')
+    r = RatingService()
+    image = r.get_image()
+    data = {
+        '_id': str(image['_id']),
+        'image': image['image'],
+    }
+    return render_template('rating/index.html', data=data)
+
+
+@rating.route('/api/skip', methods=['POST'])
+@login_required
+def skip_image():
+    if request.method == 'POST':
+        _id = request.form.get('_id')
+        if _id is None:
+            return '0'
+
+        r = RatingService()
+        skipped = r.skip_image(_id)
+        if not skipped:
+            return '0'
+        return '1'
+
+
+@rating.route('/api/rate', methods=['POST'])
+@login_required
+def rate_image():
+    if request.method == 'POST':
+        _id = request.form.get('_id')
+        rate = request.form.get('rate')
+        if not all([_id, rate]):
+            return '0'
+        r = RatingService()
+        rated = r.rate_image(_id, rate)
+        if not rated:
+            return '0'
+        return '1'
--- webapp/app/rating/services.py
+++ webapp/app/rating/services.py
@@ -1,6 +1,7 @@
 from app import mongo
 from app.users.services import UserService
 import hashlib
+from bson.objectid import ObjectId
 
 
 class RatingService:
@@ -17,3 +18,26 @@ class RatingService:
         if not q:
             return mongo.db[self.base_table].find().count()
         return mongo.db[self.base_table].find(q).count()
+
+    def get_image(self):
+        image = mongo.db[self.base_table].find_one({'is_skipped': False, 'is_labeled': False})
+        if not image:
+            return False
+        return image
+
+    def skip_image(self, _id):
+        mongo.db[self.base_table].update({'_id': ObjectId(_id)}, {'$set': {'is_skipped': True}})
+        return True
+
+    def rate_image(self, _id, rate):
+        try:
+            rate = int(rate)
+            if rate > 5:
+                rate = 5
+            mongo.db[self.base_table].update(
+                {'_id': ObjectId(_id)},
+                {'$set': {'is_labeled': True, 'rate': int(rate)}}
+            )
+            return True
+        except:
+            return False
